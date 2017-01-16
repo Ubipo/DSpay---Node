dade6b5accd21774b67b353bf6b001667ec35269
Ubipo <Ubipo.Skippy@gmail.com>
2017-01-16 20:39:09 +0100

    Testing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('joehoe');
 });
 
 app.get('/cool', function(request, response) {
